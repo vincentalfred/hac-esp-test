46d67429a70c51f23779605d7d147867ebfc52be
WjyChandra <chandrawijaya53@yahoo.co.id>
2019-03-18 11:15:33 +0700

    Edit PZEM004T.h

--- src/hac-pzem004t.h
+++ src/hac-pzem004t.h
@@ -1,4 +1,5 @@
 #include"PZEM004T.h"
+#include"hac-lcd.h";
 /*
    An example on how to use ESP8266 hardware serial with PZEM004T
    Note: ESP8266 UART0 pins GPIO1 (TX) and GPIO3 (RX) are usually connected
@@ -10,20 +11,49 @@
 */
 
 
-HardwareSerial hwserial(UART0);     // Use hwserial UART0 at pins GPIO1 (TX) and GPIO3 (RX)
-PZEM004T pzem(&hwserial);           // Attach PZEM to hwserial
-IPAddress ip(192,168,3,157);
+     								// Use hwserial UART0 at pins GPIO1 (TX) and GPIO3 (RX)
+PZEM004T pzem(3, 1);           		// Attach PZEM to hwserial
+IPAddress ip(192,168,1,1);
 
-bool pzemrdy = false;
+
 
 void setupPZEM() {
-   hwserial.swap();           // (optionally) swap hw_serial pins to gpio13(rx),15(tx)
-   Serial1.begin(115200);     /* Since UART0 is occupied with PZEM004T, we use UART1 to output data to serial monitor
-                                 UART1 uses hwserial at pin GPIO2
-                              */
-   while (!pzemrdy) {
-      Serial1.println("Connecting to PZEM...");
-      pzemrdy = pzem.setAddress(ip);
+      pzem.setAddress(ip);
+      lcdPrint("Connecting To PZEM"); 
       delay(1000);
    }
+
+
+void powerDisplay(){
+	float v = pzem.voltage(ip);
+    if (v < 0.0) v = 0.0;
+    Serial.print(v);Serial.print("V; ");
+    char buffer1[4]; 
+    String volt = dtostrf(v, 3 , 1, buffer1);  
+    lcd.setCursor(0, 0); lcd.print("V:"+volt); 
+                
+    float i = pzem.current(ip);
+    if(i >= 0.0){ Serial.print(i);Serial.print("A; "); }
+    char buffer2[4];
+    String currents = dtostrf(i, 2, 2, buffer2); 
+    lcd.setCursor(8, 0); lcd.print("A:"+currents);
+            
+    float p = pzem.power(ip);
+    if(p >= 0.0){ Serial.print(p);Serial.print("W; "); }
+    char buffer3[4]; 
+    String powers = dtostrf(p, 4, 0, buffer3); 
+    lcd.setCursor(0, 1); lcd.print("W:"+powers);
+           
+    float e = pzem.energy(ip);
+    if(e >= 0.0){ Serial.print(e);Serial.print("Wh; "); }
+    char buffer4[4]; 
+    String energys = dtostrf(e , 4, 0, buffer4); 
+    lcd.setCursor(8, 1); lcd.print("Wh:"+energys);
+
+    Serial.println();
+   
+
+  	delay(1000);
+  	lcd.clear();
+
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "hac-esp.h"
 
 void setup() {
-    Serial.begin(9600);
+    //Serial.begin(9600);
     setupLCD();
     setupMQTT();
     setupRFID();
@@ -10,6 +10,7 @@ void setup() {
 }
 
 char cur_carduid[20] = {};
+bool activate = 0; // value from cloud to ack SSR pin D8
 
 void loop(){
     client.loop();
@@ -21,26 +22,10 @@ void loop(){
         printDebug(carduid);
         String topic = String(machine_id) + "/state/carduid";
         client.publish(topic, carduid);
-    }
 
-    if (SSR_PIN == HIGH){
-        float v = pzem.voltage(ip);
-        if (v < 0.0) v = 0.0;
-         Serial1.print(v);Serial1.print("V; ");
+        if (activate == 1 ) digitalWrite(SSR_PIN, HIGH); // to turn on SSR
 
-        float i = pzem.current(ip);
-         if(i >= 0.0){ Serial1.print(i);Serial1.print("A; "); }
-
-        float p = pzem.power(ip);
-         if(p >= 0.0){ Serial1.print(p);Serial1.print("W; "); }
-
-        float e = pzem.energy(ip);
-         if(e >= 0.0){ Serial1.print(e);Serial1.print("Wh; "); }
-        // String topic = String(pzem.energy(ip)) + "/state/energy";
-        // client.publish(topic, pzem.energy(ip));
-        Serial1.println();
-
-
-      }
-    delay(1000);
-}
+        if (SSR_PIN == HIGH){
+            powerDisplay(); 
+        }
+  }
