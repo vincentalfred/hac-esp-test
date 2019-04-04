f82305da09966d94edfefe81903f5b1be1a83ed8
WjyChandra <chandrawijaya53@yahoo.co.id>
2019-04-04 16:53:57 +0700

    Fixing Disconnect Condition

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,10 @@ char cur_carduid[20] = {};
 
 void loop(){
 		client.loop();
-		if (!client.connected()) connect();
 
 		char carduid[20];
 		if (activate == 0) {
+			if (!client.connected()) connect();
 			welcomeScreen();
 			if (getCardUID(carduid) && strcmp(carduid, cur_carduid) != 0) {
 				strcpy(cur_carduid, carduid);
